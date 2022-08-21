d8a0a7efe191754b79e060a2d0e4981f83891873
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-21 17:44:40 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+//Returns the number of blocks.
+function distanceFromHqInBlocks(blocks) {
+  let blockDistance = Math.abs(blocks - 42);
+  return blockDistance;
+}
+
+//Returns the number of feet.
+function distanceFromHqInFeet(blocks) {
+    distanceFromHqInBlocks(blocks);
+    return (distanceFromHqInBlocks(blocks)*264);
+}
+
+//returns the number of feet travelled.
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start)*264;
+}
+//returns the fare for the customer.
+function calculatesFarePrice(start, destination) {
+    distanceTravelledInFeet(start, destination);
+    let totalDistance = distanceTravelledInFeet(start, destination);
+    if (totalDistance < 400) {
+        return 0;
+    }
+    if (totalDistance > 2500) {
+        return 'cannot travel that far';
+    }
+    else if (totalDistance < 2000) {
+        return (totalDistance - 400) * 0.02;
+    }
+    else {
+        return 25;
+    }
+
+}
+
